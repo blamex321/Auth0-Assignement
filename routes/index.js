ed81244ee34079c9ff258d3c12e0c7f3a96bb93d
B V S Laxman <laxman2207@outlook.com>
2023-04-02 22:14:13 +0530

    configure final routes

--- routes/index.js
+++ routes/index.js
@@ -1,50 +1,62 @@
-const router = require('express').Router();
-const { requiresAuth } = require('express-openid-connect');
-const https = require('https');
+const router = require("express").Router();
+const { requiresAuth } = require("express-openid-connect");
+const https = require("https");
 
-router.get('/', function (req, res) {
-    if(req.oidc.isAuthenticated()){
-        res.render('index',{
-            name:req.oidc.user
-        });
-    } else {
-        res.redirect("/login");
-    }
+router.get("/", function (req, res) {
+  if (req.oidc.isAuthenticated()) {
+    res.render("index", {
+      name: req.oidc.user,
+    });
+  } else {
+    res.redirect("/login");
+  }
 });
 
-router.post("/:cityName",function(req,res){
-    let city = "";
-    city = req.params.cityName;
-    if(city === "new-york"){
-        city = "new york city";
-    };
-    const apiKey = process.env.APIKEY;
-    const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q= " + city + " &units=metric";
-    https.get(url,function(response){
-        console.log(response.statusCode);
+router.get("/:cityName", function (req, res) {
+  let city = "";
+  city = req.params.cityName;
 
-        response.on("data",function(data){
-            const weatherData = JSON.parse(data);
-            // console.log(weatherData);
-            const dateOBJ = (new Date(weatherData.dt*1000+(weatherData.timezone*1000)));
-            const date = dateOBJ.getDate();
-            const month = dateOBJ.getMonth() + 1 ;
-            const year = dateOBJ.getFullYear();
+  if (city === "new-york") {
+    city = "New York";
+  }
 
-            const dateString = date + "/" + month + "/" + year;
+  const apiKey = process.env.APIKEY;
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?appid=" +
+    apiKey +
+    "&q=" +
+    city +
+    " &units=metric";
 
-            // console.log(dateString)
-            const r = "1h";
-            const weatherOBJ = {
-                temp : weatherData.main.temp,
-                humidity : weatherData.main.humidity,
-                wind : weatherData.wind.speed,
-                rain : weatherData.rain['1h'],
-                date : dateString
-            }
-            res.render("weather",{currWeather : weatherOBJ});
-        });
+  https.get(url, function (response) {
+    console.log(response.statusCode);
+    response.on("data", function (data) {
+
+      const weatherData = JSON.parse(data);
+
+      const dateOBJ = new Date(
+        weatherData.dt * 1000 + weatherData.timezone * 1000
+      );
+      const time = dateOBJ.toUTCString().substring(17,26);
+
+      const dateString = (dateOBJ.toUTCString().substring(5,16));
+      
+      let rain = "No Rain";
+      if (weatherData.rain) {
+        rain = weatherData.rain["1h"] || weatherData.rain["3h"] + "mm";
+      }
+      // console.log(dateString)
+      const weatherOBJ = {
+        temp: weatherData.main.temp,
+        humidity: weatherData.main.humidity,
+        wind: weatherData.wind.speed,
+        rain: rain,
+        date: dateString,
+        city: weatherData.name,
+        time: time
+      };
+      res.render("weather", { currWeather: weatherOBJ });
     });
-
+  });
 });
 module.exports = router;
